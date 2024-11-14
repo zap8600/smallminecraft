aee84fbebf6763311a6736e28dba5d85cd519b2b
zap8600 <95487746+zap8600@users.noreply.github.com>
2024-11-14 19:50:48 +0000

    wip

--- gettexture.py
+++ gettexture.py
@@ -1,8 +1,13 @@
 import json
 
 with open('textures.json') as json_data:
+    global d
     d = json.load(json_data)
     json_data.close()
-    for i in range(len(d)):
+    for j in range(len(d)):
         for i in range(8):
-            print(d[0][0 + i], d[0][1 + i], d[0][2 + i], d[0][3 + i], d[0][4 + i], d[0][5 + i], d[0][6 + i], d[0][7 + i])
+            print(d[j][0 + (i * 8)], d[j][1 + (i * 8)], d[j][2 + (i * 8)], d[j][3 + (i * 8)], d[j][4 + (i * 8)], d[j][5 + (i * 8)], d[j][6 + (i * 8)], d[j][7 + (i * 8)])
+
+i = 0
+j = 0x
+print(d[j][0 + (i * 8)], d[j][1 + (i * 8)], d[j][2 + (i * 8)], d[j][3 + (i * 8)], d[j][4 + (i * 8)], d[j][5 + (i * 8)], d[j][6 + (i * 8)], d[j][7 + (i * 8)])
